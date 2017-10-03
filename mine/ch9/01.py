3a3816a13a4c186afed44124627449b42cd0b2c4
장상현 <jexjang92@gmail.com>
2017-10-03 21:49:09 +0900

    커밋 요약
    ---
    What :
    [무엇을 진행했는지에 대해 리스트형태로]
    matplotlib.use('Agg') 추가
    Why :
    [왜 진행했는지에 대해 리스트 형태로]
    --- 오류떠서
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

--- mine/ch9/01.py
+++ mine/ch9/01.py
@@ -2,6 +2,8 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
